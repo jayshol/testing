5eb27c64ada04143b98ace1696fd43cf16b547bf
Jayashree Sholavandan <srjayashree@gmail.com>
2025-03-21 12:52:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import express
+const express = require('express');
+const app = express();
+
+// Define a port to listen on
+const PORT = 3000;
+
+// Add a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, world! The server is up and running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
+
